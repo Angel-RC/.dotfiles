34f47f6a50127c17a06b50b8e4915229b9c9fc34
chicote <angel.rodriguez@singularu.com>
2023-09-09 15:55:39 +0200

    mejora

---
+++ restoration_scripts/01-sample_script.sh
@@ -0,0 +1,10 @@
+echo "This is a script ran by the restoration process. You can add yours inside '$DOTFILES_PATH/restoration_scripts'"
+echo
+echo "Self install scripts must have chmod u+x to run"
+echo
+cargo install eza
+sudo apt install glances
+sudo apt install fd-find
+# Download Oh My Posh
+sudo apt install unzip
+curl -s https://ohmyposh.dev/install.sh | sudo bash -s
---
+++ shell/functions.sh
@@ -0,0 +1,76 @@
+function cdd() {
+	cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
+}
+
+function z() {
+	fname=$(declare -f -F _z)
+
+	[ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
+
+	_z "$1"
+}
+
+function recent_dirs() {
+	# This script depends on pushd. It works better with autopush enabled in ZSH
+	escaped_home=$(echo $HOME | sed 's/\//\\\//g')
+	selected=$(dirs -p | sort -u | fzf)
+
+	cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+}
+
+
+cecho () {
+ 
+    declare -A colors;
+    colors=(\
+        ['red']='\033[31m'\
+        ['green']='\033[32m'\
+        ['yellow']='\033[0;33m'\
+        ['blue']='\033[34m'\
+        ['magenta']='\033[35m'\
+        ['cyan']='\033[36m'\
+        ['white']='\033[0;37m'\
+		['orange']='\033[33m'\
+		['default']='\033[39m'\
+    );
+ 
+    local defaultMSG="No message passed.";
+    local defaultColor="default";
+    local defaultNewLine=true;
+ 
+    while [[ $# -gt 1 ]];
+    do
+    key="$1";
+ 
+    case $key in
+        -c|--color)
+            color="$2";
+            shift;
+        ;;
+    esac
+    shift;
+    done
+ 
+    message=${1:-$defaultMSG};   # Defaults to default message.
+    color=${color:-$defaultColor};   # Defaults to default color, if not specified.
+	
+    echo -e "${colors[$color]}" "$@";
+    tput sgr0; #  Reset text attributes to normal without clearing screen.
+ 
+    return;
+}
+ 
+warning () {
+ 
+    cecho -c 'yellow' "$@";
+}
+ 
+error () {
+ 
+    cecho -c 'red' "$@";
+}
+ 
+information () {
+ 
+    cecho -c 'blue' "$@";
+}
